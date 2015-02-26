3be64810064c2fed79f472ae7e53104f6ab8c8b7
student <student@TSL-MBP-01.local>
2015-02-26 13:43:36 -0600

    adds faker gem and seed file for users

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker'
 gem 'devise'
 gem 'starter_generators', :git => 'git://github.com/rbetina/starter_generators', :branch => 'panels'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  u = User.new
+  u.email = Faker::Internet.safe_email
+  u.password = "12341234"
+  u.password_confirmation = "12341234"
+  u.save
+end
+
+
+
+
+
+# NORMAL: type in user email in form -> gets saved into email column in database
+# PASSWORD FLOW IN DEVISE: type in password in the form
+                              # -> devise processes the password and password confirmation into an encrypted hash
+                              # -> devise saves the encyrpted hash into the "encrypted_password" column
