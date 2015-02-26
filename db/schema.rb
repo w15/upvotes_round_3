5e6fac66376fec6ef444b8e53a33a9d17a737933
student <student@TSL-MBP-01.local>
2015-02-26 14:04:27 -0600

    adds upvote model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226191611) do
+ActiveRecord::Schema.define(version: 20150226200414) do
 
   create_table "items", force: :cascade do |t|
     t.string   "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "upvotes", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
