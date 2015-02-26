d338aaebc3d9f3891f02aeb00b1eac76666c5c0d
student <student@TSL-MBP-01.local>
2015-02-26 14:48:41 -0600

    add form_tag for new upvote form, adds show action

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,4 +2,20 @@ class UpvotesController < ApplicationController
   def new
 
   end
+
+  def create
+    u = Upvote.new
+    u.item_id = params[:item_id]
+    u.user_id = params[:user_id]
+    u.save
+
+    redirect_to upvote_url(u.id)
+
+    # Could also be written as:
+    # redirect_to "upvotes/#{ u.id }"
+  end
+
+  def show
+    @upvote = Upvote.find(params[:id])
+  end
 end
